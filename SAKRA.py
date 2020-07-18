Now supports grouped numerical phenotype (e.g. protein level); fixed error occurs when reconstruction loss is backwarded but not all pheno&signature selected; ToTensor allows specifying tensor type; FCRegressor supports specifying number of hidden neurons; SAKRA main supports handling manual splits

SAKRA.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import pickle
 import random
 import warnings
 
@@ -184,8 +185,21 @@ class SAKRA(object):
         self.splits = dict()
         self.data_splitter = DataSplitter()
 
-        # Dummy 'all' split mask
+        # 'all' split mask for selecting all data
         self.splits['all'] = np.ones(len(self.count_data), dtype=np.bool)
+
+        if self.config.get('manual_split') == 'True':
+            # Should load external files for splits
+            # when conflicts occur, will directly override existing splits (i.e. all)
+            with open(self.config.get('manual_split_pkl_path'), 'rb') as f:
+                ext_splits = pickle.load(f)
+            for cur_split_key in ext_splits.keys():
+                self.splits[cur_split_key] = ext_splits[cur_split_key]
+            if self.verbose:
+                print('=================')
+                print("Imported external splits from", self.config.get('manual_split_pkl_path'))
+                print("External splits:", ext_splits)
+                print("Merged splits:", self.splits)
 
         ## Overall train/test split
         if self.config['overall_train_test_split']['type'] == 'auto':
@@ -199,8 +213,11 @@ class SAKRA(object):
                                                                                            k=self.split_overall_train_dec)
             self.splits['overall_train'] = overall_train_test_split['train'].astype(np.bool)
             self.splits['overall_test'] = overall_train_test_split['test'].astype(np.bool)
+        elif self.config['overall_train_test_split']['type'] == 'none':
+            # Do nothing
+            if self.verbose:
+                print('Skipped main splits')
         else:
-            # TODO: Manual overall train/test split
             raise NotImplementedError
 
         ## Phenotype train/test
@@ -208,6 +225,7 @@ class SAKRA(object):
             for cur_pheno in self.selected_pheno:
                 # Auto split
                 if self.count_data.pheno_meta[cur_pheno]['split']['type'] == 'auto':
+
                     train_split_id = 'pheno_' + str(cur_pheno) + '_train'
                     test_split_id = 'pheno_' + str(cur_pheno) + '_test'
                     cur_base_split = self.splits[self.count_data.pheno_meta[cur_pheno]['split']['base']].astype(
@@ -221,8 +239,11 @@ class SAKRA(object):
                         k=self.count_data.pheno_meta[cur_pheno]['split']['train_dec'])
                     self.splits[train_split_id] = cur_label_train_test_split['train'].astype(np.bool)
                     self.splits[test_split_id] = cur_label_train_test_split['test'].astype(np.bool)
+                elif self.count_data.pheno_meta[cur_pheno]['split']['type'] == 'none':
+                    # Do nothing
+                    if self.verbose:
+                        print('Skipped pheno splits for:', cur_pheno)
                 else:
-                    # TODO: Manual label train/test split
                     raise NotImplementedError
 
         ## Signature train/test
@@ -244,8 +265,11 @@ class SAKRA(object):
                         k=self.signature_config[cur_signature]['split']['train_dec'])
                     self.splits[train_split_id] = cur_signature_train_test_split['train'].astype(np.bool)
                     self.splits[test_split_id] = cur_signature_train_test_split['test'].astype(np.bool)
+                elif self.signature_config[cur_signature]['split']['type'] == 'none':
+                    # Do nothing
+                    if self.verbose:
+                        print('Skipped signature splits for:', cur_signature)
                 else:
-                    # TODO: Manual signature train/test split
                     raise NotImplementedError
 
         ## Print splits for debugging (in verbose mode)
@@ -274,8 +298,16 @@ class SAKRA(object):
         # Check pheno_df_keys of selected phenotypes are exist in the dataset
         problematic_phenos = list()
         for cur_pheno in self.selected_pheno:
-            cur_pheno_df_key = pheno_meta[cur_pheno]['pheno_df_key']
-            if (cur_pheno_df_key in self.count_data.pheno_df.columns) is False:
+            if pheno_meta[cur_pheno]['type'] == 'categorical':
+                cur_pheno_df_key = pheno_meta[cur_pheno]['pheno_df_key']
+                if (cur_pheno_df_key in self.count_data.pheno_df.columns) is False:
+                    problematic_phenos.append(cur_pheno)
+            elif pheno_meta[cur_pheno]['type'] == 'numerical':
+                cur_pheno_df_keys = pheno_meta[cur_pheno]['pheno_df_keys']
+                for cur_key in cur_pheno_df_keys:
+                    if (cur_key in self.count_data.pheno_df.columns) is False:
+                        problematic_phenos.append(cur_pheno)
+            else:
                 problematic_phenos.append(cur_pheno)
         if len(problematic_phenos) > 0:
             warnings.warn("Exists selecting phenotype(s) not found in the dataset:" + str(problematic_phenos))

model_controllers/extractor_controller.py
@@ -411,15 +411,27 @@ class ExtractorController(object):
         :return:
         """
         # Forward model (obtain pre-loss-calculated tensors)
-        fwd_res = self.model(batch['expr'][expr_key],
-                             forward_main_latent=forward_main_latent,
-                             forward_reconstruction=forward_reconstruction,
-                             forward_signature=forward_signature,
-                             selected_signature=self.select_item_dict(selection=selected_signature,
-                                                                      internal=self.signature_config),
-                             forward_pheno=forward_pheno,
-                             selected_pheno=self.select_item_dict(selection=selected_pheno,
-                                                                  internal=self.pheno_config))
+        if forward_reconstruction:
+            # When reconstruction is on, all latents should be forwarded, though loss could be backwarded partially
+            fwd_res = self.model(batch['expr'][expr_key],
+                                 forward_main_latent=forward_main_latent,
+                                 forward_reconstruction=forward_reconstruction,
+                                 forward_signature=forward_signature,
+                                 selected_signature=self.select_item_dict(selection='*',
+                                                                          internal=self.signature_config),
+                                 forward_pheno=forward_pheno,
+                                 selected_pheno=self.select_item_dict(selection='*',
+                                                                      internal=self.pheno_config))
+        else:
+            fwd_res = self.model(batch['expr'][expr_key],
+                                 forward_main_latent=forward_main_latent,
+                                 forward_reconstruction=forward_reconstruction,
+                                 forward_signature=forward_signature,
+                                 selected_signature=self.select_item_dict(selection=selected_signature,
+                                                                          internal=self.signature_config),
+                                 forward_pheno=forward_pheno,
+                                 selected_pheno=self.select_item_dict(selection=selected_pheno,
+                                                                      internal=self.pheno_config))
 
         # Reconstruction Loss
         main_loss = {'loss': dict(), 'regularization': dict()}
@@ -468,6 +480,9 @@ class ExtractorController(object):
                     elif cur_pheno_loss['type'] == 'MSE' or cur_pheno_loss['type'] == 'L2':
                         pheno_loss[cur_pheno]['loss'][cur_pheno_loss_key] = \
                             torch.nn.functional.mse_loss(fwd_res['pheno_out'][cur_pheno], pheno_ans)
+                    elif cur_pheno_loss['type'] == 'L1':
+                        pheno_loss[cur_pheno]['loss'][cur_pheno_loss_key] = \
+                            torch.nn.functional.l1_loss(fwd_res['pheno_out'][cur_pheno], pheno_ans)
                     else:
                         print('Unsupported phenotype supervision loss type.')
                         raise ValueError

models/extractor.py
@@ -60,7 +60,8 @@ class Extractor(torch.nn.Module):
                     if model_details['type'] == 'FCRegressor':
                         signature_regressors[cur_signature] = model.FCRegressor(
                             input_dim=self.signature_config[cur_signature]['signature_lat_dim'],
-                            output_dim=self.signature_config[cur_signature]['siganture_out_dim']
+                            output_dim=self.signature_config[cur_signature]['siganture_out_dim'],
+                            hidden_neurons=self.signature_config[cur_signature].get('hidden_neurons', 5)
                         )
                     elif model_details['type'] == 'LinRegressor':
                         signature_regressors[cur_signature] = model.LinRegressor(
@@ -106,7 +107,8 @@ class Extractor(torch.nn.Module):
                     elif model_details['type'] == 'FCRegressor':
                         pheno_models[cur_pheno] = model.FCRegressor(
                             input_dim=self.pheno_config[cur_pheno]['pheno_lat_dim'],
-                            output_dim=self.pheno_config[cur_pheno]['pheno_out_dim']
+                            output_dim=self.pheno_config[cur_pheno]['pheno_out_dim'],
+                            hidden_neurons=self.pheno_config[cur_pheno].get('hidden_neurons', 5)
                         )
                     elif model_details['type'] == 'LinRegressor':
                         pheno_models[cur_pheno] = model.LinRegressor(

utils/data_transformations.py
@@ -9,24 +9,34 @@ class ToTensor(object):
     Convert object to PyTorch Tensors
     """
 
-    def __call__(self, sample, input_type='gene'):
+    def __call__(self, sample, input_type='gene', force_tensor_type=None):
+        ret = None
         if input_type == 'gene':
             if type(sample) is pd.core.frame.DataFrame:
-                return torch.from_numpy(sample.astype(np.float).values).transpose(0, 1).float()
+                ret = torch.from_numpy(sample.astype(np.float).values).transpose(0, 1).float()
             elif type(sample) is pd.core.series.Series:
-                return torch.from_numpy(sample.astype(np.float).values).unsqueeze(0).float()
+                ret = torch.from_numpy(sample.astype(np.float).values).unsqueeze(0).float()
             else:
                 raise NotImplementedError
         elif input_type == 'pheno':
             if type(sample) is pd.core.frame.DataFrame:
-                return torch.from_numpy(sample.astype(np.float).values)
+                ret = torch.from_numpy(sample.astype(np.float).values)
             elif type(sample) is np.ndarray:
-                return torch.from_numpy(sample)
+                ret = torch.from_numpy(sample)
             else:
                 raise NotImplementedError
         else:
             raise NotImplementedError
-
+        if force_tensor_type is not None:
+            if force_tensor_type == 'float':
+                ret = ret.float()
+            elif force_tensor_type == 'int':
+                ret = ret.int()
+            elif force_tensor_type == 'double':
+                ret = ret.double()
+            else:
+                raise NotImplementedError('Expected tensor type not supported yet')
+        return ret
 
 class ToOnehot(object):
     """